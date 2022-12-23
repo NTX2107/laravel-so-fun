f59302673d70334e99c8a5536f230927caa0680b
NTX2107 <nguyen.tx2107@gmail.com>
2022-12-23 16:52:50 +0700

    Update crud for category.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\CategoryRequest;
+use App\Services\Interfaces\ICategoryService;
 
 class CategoryController extends Controller
 {
-    //
+    private ICategoryService $categoryService;
+
+    public function __construct(ICategoryService $categoryService) {
+        $this->categoryService = $categoryService;
+    }
+
+    public function getListCategories() {
+        return $this->categoryService->findAll();
+    }
+
+    public function editCategory($id, CategoryRequest $request) {
+        $validated = $request->validated();
+        return $this->categoryService->update($id, $validated);
+    }
+
+    public function deleteCategory($id) {
+        $this->categoryService->softDelete($id);
+    }
 }
